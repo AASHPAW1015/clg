2ff3d9943ca60728d24175af7bd9a2966b9ffdee
AASHPAW1015 <aashpawcode@gmail.com>
2026-01-22 14:07:42 +0530

    array js

---
+++ jscr/jscrt/external/array1/array.js
@@ -0,0 +1,12 @@
+let student = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+let marks = [1,2,3,4,5,6,7,8,9,10];
+
+let sum = 0;
+
+for (let i of marks) {
+  sum+=i;
+}
+
+let avg = sum/marks.length;
+
+console.log("The average of the student's marks is: ", avg);
